a499b1a846499abd42c166ecaf2fb02977c4b288
Keiraz24 <113703994+Keiraz24@users.noreply.github.com>
2023-10-18 09:00:49 -0400

    second

--- jsquestions.js
+++ jsquestions.js
@@ -1,1 +1,26 @@
 console.log("Hellow world");
+
+console.log("question 1:Determine if a randomly generated number is divisible by 7 and 9. Print success and failure messages to the console.");
+var randomNumber= Math.floor(Math.random()*1000);
+console.log(randomNumber);
+if (randomNumber % 7==0 && randomNumber % 9==0){
+    console.log("sucess");
+}else{
+    console.log("failer");
+}
+
+console.log("question 2:Use the question mark and conditional chaining to determine a student's letter grade based on a number");
+
+const grade =95 ;
+var realGrade = grade >= 70 ? grade >=80 ? grade >= 90 ? "A" : "B" : "C": "F";
+console.log(realGrade); // 
+
+console.log("Make a function that does an arithmetic calculation like addition or calculates gravity. The function must have parameters and returns. Console.log the result of your calculation");
+
+
+function poundsOnNeptune(x){
+    return (x/9.81)* 11.15;
+}
+
+let x= poundsOnNeptune(140);
+console.log(x)
